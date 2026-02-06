ba67790fe74015acfb67be9da8defab64e7c151d
Devpriyam Agarwal <agarwaldevpriyam02@gmail.com>
2026-02-06 12:52:34 +0000

    container config done

---
+++ basics/conversion_datatype.js
@@ -0,0 +1,12 @@
+let marks = 45
+
+console.log(typeof marks);
+
+let valueInNumber = Number(marks)
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+//Number or any data type after declare it shows on that datatype only and it shows only type value is Nan.
+//boolean datatype 1=>true, 0=>false, ""=>false, "hii"=>true.
---
+++ basics/operations_datatype.js
@@ -0,0 +1,14 @@
+//(+, -, *, /, **, %)(add, sub, mul, div, power, modulous)
+
+let value = 7
+let negvalue = -value
+
+console.log(negvalue);
+
+let a = "Hii I am "
+let b = " Dev"
+
+let c = a+b
+
+console.log(c);
+// in this the "3" + 2 = 32, 2+"3" = 23, 1+2+"3"=33, "3"+2+1 = 321. 
