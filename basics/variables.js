3396d04ded0519f2427f2053113df293c2733eb8
Devpriyam Agarwal <agarwaldevpriyam02@gmail.com>
2026-02-06 12:00:30 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 2121 //these are not update in any situation due to const keyword.
+let accountEmail = "xyz2gmail.com"
+var accountPass = "12334" //dont use var in javascript
+accountCity = "Meerut"
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
