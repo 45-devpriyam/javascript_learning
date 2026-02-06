7856cb28c676121eb4edecfb98c5492ecf8c7061
Devpriyam Agarwal <agarwaldevpriyam02@gmail.com>
2026-02-06 12:19:37 +0000

    container config done

---
+++ basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as a newer version
+
+//alert("Hello") // It shows error because we run nodejs not browser
+
+
+console.log(3+3);
+
+/*
+number, string, boolean, undefined, symbol
+*/
+console.log(typeof 3+3);
+//typeof to know the data type in javascript.
