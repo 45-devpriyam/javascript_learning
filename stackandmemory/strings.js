70c90d9f6711a05efd879e0c2e2e455fed8087b9
Devpriyam Agarwal <agarwaldevpriyam02@gmail.com>
2026-02-07 12:42:00 +0000

    container config done

---
+++ stackandmemory/strings.js
@@ -0,0 +1,19 @@
+const name = "Rahul"
+const RepoCount = 2
+
+console.log(`My name is ${name} and my repo count is ${RepoCount}`)
+
+const secname = new String("Govind")
+console.log(secname);
+
+console.log(secname.toUpperCase());
+console.log(secname.charAt(5));
+console.log(secname.indexOf('i'));
+console.log(secname.substring(0,5));
+console.log(secname.slice(-8, 3));
+//trim is a part of string function where we can remove space fro te string it has off two types start, end.
+
+
+
+
+
