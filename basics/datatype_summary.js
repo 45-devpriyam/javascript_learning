731856b73fde2155f9aee4296bf9a5cf05f9818a
Devpriyam Agarwal <agarwaldevpriyam02@gmail.com>
2026-02-07 11:52:17 +0000

    container config done

---
+++ basics/datatype_summary.js
@@ -0,0 +1,18 @@
+//primitive - 7 types
+
+//string, number, boolean, null, undefined, bigInt, symbol
+
+//reference (Non primitive)
+
+//array, object, function
+
+const score = 100
+let name = "Rahul"
+let isloggedIn = true
+let userEmail;
+let userPass = null
+
+const Id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(Id === anotherId);
